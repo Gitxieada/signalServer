cd91ada019895e879f9747160ba385345d74dd51
XieHome <qingkong_941827@163.com>
2023-07-04 15:32:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# 参考： 
+[WebRTC 从实战到未来！前端如何实现一个最简单的音视频通话？](https://juejin.cn/post/7165539003465531399#)
+
+[基于 WebRTC 的 P2P 文件传输 ](https://juejin.cn/post/7171836076246433799)
+
+[webRTC 局域网点对点文件传输测试](https://github.com/L-orz/webRTC-test)
+
+[信令服务器搭建部署](https://juejin.cn/post/7170767923005358094)
+
+[**webRTC 局域网点对点文件传输测试](https://github.com/L-orz/webRTC-test/)
+
+
+后端使用express、socket.io提供信令服务
+
+## Install dependencies
+
+```bash
+$ npm i
+```
+# 运行：
+ ##　本地运行该信令服务
+
+Start the dev server,
+
+```bash
+$ npm run start
+```
+## pkg将NodeJS项目打包，编译成跨平台的可执行文件
+
+[pkg](https://www.npmjs.com/package/pkg) 是一个可以将nodejs程序打包成exe文件的工具，免去我们的nodejs程序在客户电脑上跑时需要客户下载node环境。不仅可以打包成exe在window上跑，还可以打包成linux/mac下的可执行程序
+```bash
+$ npm run pkg
+```
+双击exe文件即可启动服务
+
+ 在前端页面：https://gitxieada.github.io/Demo/webrtc/index.html#/signal，输入信令服务器的地址signalServer，默认'ws://ip地址:5000'；注意在电脑测试时需要填 ws://localhost:5000
+
+ 前端代码在https://github.com/Gitxieada/Demo/tree/master/webrtc，可以下载下来在本地部署
+ 
---
+++ src/index.ts
@@ -0,0 +1,93 @@
+import  express, { Application } from 'express'
+// https://stackoverflow.com/questions/63320525/error-ts1259-module-node-modules-types-express-index-can-only-be-default
+import { Server as SocketioServer } from 'socket.io'
+import { createServer, Server as HTTPServer } from 'http'
+const DEFAULT_PORT = 5000
+
+export class Server {
+    private io: SocketioServer
+    private httpServer: HTTPServer
+    private app: Application
+  
+    constructor() {
+      this.initialize()
+      this.handleSocketConnection()
+    }
+  
+    private initialize(): void {
+      this.app = express()
+      this.httpServer = createServer(this.app)
+      // https://socket.io/docs/v4/server-initialization/
+      this.io = new SocketioServer(this.httpServer, { cors: { origin: '*' } })
+    }
+  
+    private handleSocketConnection() {
+      handleSocketConnection(this.io)
+    }
+  
+    public listen(port?: number) {
+      const _port = port || DEFAULT_PORT
+      this.httpServer.listen(_port, () => {
+        console.log(`Server is listening on [Port: ${_port}]`)
+      })
+    }
+  }
+  
+  function handleSocketConnection(io: SocketioServer) {
+    let activeSocketIds: string[] = [] // 当前连接的 socket
+  
+    io.on('connection', (socket) => {
+      // 如果已存在 id
+      if (activeSocketIds.some((id) => id === socket.id)) {
+        return
+      }
+  
+      // 记录新的 socket
+      activeSocketIds.push(socket.id)
+      console.log(`Socket connected. [SocketID: ${socket.id}]`)
+  
+      // 推送用户列表
+      io.emit('update-users', {
+        users: activeSocketIds,
+      })
+  
+      // 连接断开
+      socket.on('disconnect', () => {
+        // 从记录中移除
+        activeSocketIds = activeSocketIds.filter((id) => id !== socket.id)
+        console.log(`Socket disconnected. [SocketID: ${socket.id}]`)
+  
+        // 推送用户列表
+        socket.broadcast.emit('update-users', { users: activeSocketIds })
+      })
+  
+      // 监听 call-user 并通知对应用户，将 offer 及 id 发送给用户
+      socket.on('call-user', ({ offer, to }) => {
+        console.log('call-user', to)
+        socket.to(to).emit('call', {
+          offer,
+          user: socket.id,
+        })
+      })
+  
+      socket.on('answer', ({ answer, to }) => {
+        console.log('answer', to)
+        socket.to(to).emit('answer', {
+          answer,
+          user: socket.id,
+        })
+      })
+  
+      socket.on('candidate', ({ candidate, to }) => {
+        console.log('candidate to', to)
+        socket.to(to).emit('candidate', {
+          candidate,
+          user: socket.id,
+        })
+      })
+    })
+  }
+  
+const server = new Server()
+
+server.listen()
